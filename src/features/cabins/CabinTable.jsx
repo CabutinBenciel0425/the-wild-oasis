0320380210b02194b5adc46525729d37e2c4b7e1
Benciel Cabutin <cabutinb@gmail.com>
2026-03-20 01:37:11 +0800

    Added a reusable filter button on Cabins page

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,4 +1,5 @@
 import { useCabins } from "./useCabins";
+import { useSearchParams } from "react-router-dom";
 
 import Spinner from "../../ui/Spinner";
 import CabinRow from "./CabinRow";
@@ -7,8 +8,19 @@ import Menus from "../../ui/Menus";
 
 export default function CabinTable() {
   const { isLoading, cabins } = useCabins();
+  const [searchParams] = useSearchParams();
 
   if (isLoading) return <Spinner />;
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filteredCabins;
+
+  if (filterValue === "all") filteredCabins = cabins;
+  if (filterValue === "no-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount === 0);
+  if (filterValue === "with-discount")
+    filteredCabins = cabins.filter((cabin) => cabin.discount > 0);
 
   return (
     <Menus>
@@ -23,7 +35,7 @@ export default function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          data={filteredCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
       </Table>
