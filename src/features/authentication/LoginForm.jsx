879a5ed601817e4ef2be20bf33c2d286187601c4
Benciel Cabutin <cabutinb@gmail.com>
2026-03-25 12:39:00 +0800

    Added a feature for protecting routes

--- src/features/authentication/LoginForm.jsx
+++ src/features/authentication/LoginForm.jsx
@@ -17,7 +17,14 @@ function LoginForm() {
 
     if (!email || !password) return;
 
-    login({ email, password });
+    login(
+      { email, password },
+      {
+        onSettled: () => {
+          setPassword("");
+        },
+      },
+    );
   }
 
   return (
