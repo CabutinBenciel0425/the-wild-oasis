2cf1a30f119f74f08a3c216f49b8ccbebc4d652a
Benciel Cabutin <cabutinb@gmail.com>
2026-03-25 13:00:31 +0800

    Added logout feature

---
+++ src/features/authentication/Logout.jsx
@@ -0,0 +1,14 @@
+import { HiArrowRightEndOnRectangle } from "react-icons/hi2";
+import { useLogout } from "./userLogout";
+
+import ButtonIcon from "../../ui/ButtonIcon";
+import SpinnerMini from "../../ui/SpinnerMini";
+
+export default function Logout() {
+  const { logout, isLoggingOut } = useLogout();
+  return (
+    <ButtonIcon onClick={logout} disabled={isLoggingOut}>
+      {!isLoggingOut ? <HiArrowRightEndOnRectangle /> : <SpinnerMini />}
+    </ButtonIcon>
+  );
+}
